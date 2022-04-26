ee66fb415658e4cd9aa23e0d0bc9d4806327daa5
zsw <546232194@qq.com>
2022-04-26 17:14:59 +0800

    love

---
+++ src/main/java/com/thread/demoPro/Constant.java
@@ -0,0 +1,21 @@
+package com.thread.demoPro;
+
+import java.awt.*;
+
+/**
+ * @author zsw
+ * @date 2022/4/26 17:11
+ * @Company: 广州市两棵树网络科技有限公司
+ */
+public class Constant {
+    static final int WIDTH = 800;
+    static final int HEIGHT = 450;
+    static final int X = (Toolkit.getDefaultToolkit().getScreenSize().width - WIDTH) / 2;
+    static final int Y = (Toolkit.getDefaultToolkit().getScreenSize().height - HEIGHT) / 2;
+    static final int WIDTH_BUFF = 300;
+    static final int HEIGHT_BUFF = 100;
+    static final int X_BUFF = (WIDTH - WIDTH_BUFF) / 2;
+    static final int Y_BUFF = (HEIGHT - HEIGHT_BUFF) / 2;
+    static final String string = "I LOVE YOU !";
+    static final String string1 = "★ 一生一世 ★";
+}
